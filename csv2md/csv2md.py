8ec23e5f1f7318e7de9940f038c28a701b00bbe7
Lev Zakharov <l.j.zakharov@gmail.com>
2018-02-04 02:43:03 +0300

    Init application

---
+++ csv2md/csv2md.py
@@ -0,0 +1,17 @@
+import sys
+
+
+def csv_to_table(file, delimiter=',', quotechar='"'):
+    pass
+
+
+def table_to_md(table):
+    pass
+
+
+def main(args):
+    pass
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
