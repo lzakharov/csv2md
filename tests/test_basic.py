6c294d247a720402a97ed388c5ee166e349b7249
Lev Zakharov <l.j.zakharov@gmail.com>
2018-02-04 03:39:13 +0300

    Fix test data

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -24,7 +24,7 @@ normal_md = (
     '| ---- | ----- | -------------------------- | --------------------------------- | ------- |\n'
     '| 1997 | Ford  | E350                       | ac, abs, moon                     | 3000.00 |\n'
     '| 1999 | Chevy | Venture «Extended Edition» |                                   | 4900.00 |\n'
-    '| 1996 | Jeep  | Grand Cherokee             | MUST SELL! air, moon roof, loaded | 4799.00 |\n'
+    '| 1996 | Jeep  | Grand Cherokee             | MUST SELL! air, moon roof, loaded | 4799.00 |'
 )
 
 
