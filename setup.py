7793ef8d2575274b0a96d2c75de8797738d95f2c
Захаров Лев Юрьевич <l.j.zakharov@gmail.com>
2019-12-09 18:59:43 +0300

    Update version to v1.1.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('LICENSE') as f:
 
 setup(
     name='csv2md',
-    version='1.0.0',
+    version='1.1.0',
     description='Command line tool for converting CSV files into Markdown tables.',
     long_description=readme,
     author='Lev Zakharov',
