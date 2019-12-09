ee6dbb3462f4d19c0d929103f53d6a9976a6d854
Захаров Лев Юрьевич <l.j.zakharov@gmail.com>
2019-12-09 18:55:12 +0300

    Add alignment and do refactoring

--- setup.py
+++ setup.py
@@ -9,10 +9,9 @@ with open('LICENSE') as f:
 
 setup(
     name='csv2md',
-    version='1.0.1',
+    version='1.0.0',
     description='Command line tool for converting CSV files into Markdown tables.',
     long_description=readme,
-    long_description_content_type='text/markdown',
     author='Lev Zakharov',
     author_email='l.j.zakharov@gmail.com',
     url='https://github.com/lzakharov/csv2md',
@@ -20,7 +19,7 @@ setup(
     packages=find_packages(exclude=('tests', 'docs')),
     entry_points={
         'console_scripts': [
-            'csv2md = csv2md:main'
+            'csv2md = csv2md.__main__:main'
         ]
     }
 )
