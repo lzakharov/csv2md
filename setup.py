133c8781df63d20e9e35d514218d6c36582d97fe
Захаров Лев Юрьевич <l.j.zakharov@gmail.com>
2019-12-09 20:59:35 +0300

    fix setup configuration

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup, find_packages
 
-
-with open('README.md') as f:
-    readme = f.read()
+readme = """
+Command line tool for converting CSV files into Markdown tables.
+""".strip()
 
 with open('LICENSE') as f:
     license = f.read()
 
 setup(
     name='csv2md',
-    version='1.1.0',
+    version='1.1.1',
     description='Command line tool for converting CSV files into Markdown tables.',
     long_description=readme,
     author='Lev Zakharov',
