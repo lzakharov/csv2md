5820d723c6c97a29aed4c2ba61a2d27948ef865f
Lev Zakharov <zakharov.lyu@sbermarket.ru>
2024-01-06 18:25:10 +0300

    release 1.3.0

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+# 1.3.0 (January 6, 2024)
+
+* Support `--column` flag for filtering columns (#15)
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('LICENSE') as f:
 
 setup(
     name='csv2md',
-    version='1.2.0',
+    version='1.3.0',
     description='Command line tool for converting CSV files into Markdown tables.',
     long_description=readme,
     author='Lev Zakharov',
