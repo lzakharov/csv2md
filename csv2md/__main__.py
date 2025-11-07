42663f3c51463c01b0acc1b155b3063844b1044e
Лев Захаров <l.j.zakharov@gmail.com>
2025-11-07 14:25:28 +0300

    ci: switch from travis to github actions

--- csv2md/__main__.py
+++ csv2md/__main__.py
@@ -7,21 +7,64 @@ from .exceptions import BaseError, ColumnIdentifierError
 
 def main():
     parser = argparse.ArgumentParser(
-        description='Parse CSV files into Markdown tables.')
-    parser.add_argument('files', metavar='CSV_FILE', type=argparse.FileType('r'), nargs='*',
-                        help='One or more CSV files to parse')
-    parser.add_argument('-d', '--delimiter', metavar='DELIMITER', type=str, default=',',
-                        help='delimiter character. Default is \',\'')
-    parser.add_argument('-q', '--quotechar', metavar='QUOTECHAR', type=str, default='"',
-                        help='quotation character. Default is \'"\'')
-    parser.add_argument('-C', '--columns', dest='columns', type=str, default=None,
-                        help='comma-separated list of column indices or ranges (from zero) to be processed, e.g. "0,3-5,7". Indices out of range will be ignored')
-    parser.add_argument('-c', '--center-aligned-columns', metavar='CENTER_ALIGNED_COLUMNS', nargs='*',
-                        type=int, default=[], help='column numbers with center alignment (from zero)')
-    parser.add_argument('-r', '--right-aligned-columns', metavar='RIGHT_ALIGNED_COLUMNS', nargs='*',
-                        type=int, default=[], help='column numbers with right alignment (from zero)')
-    parser.add_argument('-H', '--no-header-row', dest='no_header_row', action='store_true',
-                        help='specify that the input CSV file has no header row. Will create default headers in Excel format (a,b,c,...)')
+        description="Parse CSV files into Markdown tables."
+    )
+    parser.add_argument(
+        "files",
+        metavar="CSV_FILE",
+        type=argparse.FileType("r"),
+        nargs="*",
+        help="One or more CSV files to parse",
+    )
+    parser.add_argument(
+        "-d",
+        "--delimiter",
+        metavar="DELIMITER",
+        type=str,
+        default=",",
+        help="delimiter character. Default is ','",
+    )
+    parser.add_argument(
+        "-q",
+        "--quotechar",
+        metavar="QUOTECHAR",
+        type=str,
+        default='"',
+        help="quotation character. Default is '\"'",
+    )
+    parser.add_argument(
+        "-C",
+        "--columns",
+        dest="columns",
+        type=str,
+        default=None,
+        help='comma-separated list of column indices or ranges (from zero) to be processed, e.g. "0,3-5,7". Indices out of range will be ignored',
+    )
+    parser.add_argument(
+        "-c",
+        "--center-aligned-columns",
+        metavar="CENTER_ALIGNED_COLUMNS",
+        nargs="*",
+        type=int,
+        default=[],
+        help="column numbers with center alignment (from zero)",
+    )
+    parser.add_argument(
+        "-r",
+        "--right-aligned-columns",
+        metavar="RIGHT_ALIGNED_COLUMNS",
+        nargs="*",
+        type=int,
+        default=[],
+        help="column numbers with right alignment (from zero)",
+    )
+    parser.add_argument(
+        "-H",
+        "--no-header-row",
+        dest="no_header_row",
+        action="store_true",
+        help="specify that the input CSV file has no header row. Will create default headers in Excel format (a,b,c,...)",
+    )
     args = parser.parse_args()
 
     try:
@@ -31,7 +74,13 @@ def main():
 
     for file in [sys.stdin] if not args.files else args.files:
         table = Table.parse_csv(file, args.delimiter, args.quotechar, columns)
-        print(table.markdown(args.center_aligned_columns, args.right_aligned_columns, args.no_header_row))
+        print(
+            table.markdown(
+                args.center_aligned_columns,
+                args.right_aligned_columns,
+                args.no_header_row,
+            )
+        )
 
 
 def parse_columns(columns):
@@ -40,10 +89,10 @@ def parse_columns(columns):
 
     result = []
 
-    for c in columns.split(','):
-        if '-' in c:
+    for c in columns.split(","):
+        if "-" in c:
             try:
-                a, b = map(int, c.split('-', 1))
+                a, b = map(int, c.split("-", 1))
             except ValueError:
                 raise ColumnIdentifierError(c)
 
@@ -58,5 +107,5 @@ def parse_columns(columns):
     return result
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- csv2md/test_utils.py
+++ csv2md/test_utils.py
@@ -5,6 +5,6 @@ from .utils import column_letter
 
 class TestUtils(TestCase):
     def test_column_letter(self):
-        self.assertEqual(column_letter(0), 'a')
-        self.assertEqual(column_letter(4), 'e')
-        self.assertEqual(column_letter(30), 'ee')
+        self.assertEqual(column_letter(0), "a")
+        self.assertEqual(column_letter(4), "e")
+        self.assertEqual(column_letter(30), "ee")
